70e1f5751e0c75793cd6ceeb22e09f13634d2c21
Bastien Clément <g@ledric.me>
2017-11-26 23:36:37 +0100

    Add color gradient

--- Format.lua
+++ Format.lua
@@ -59,3 +59,20 @@ function Oken:Wrap(str, length)
 	end
 	return wrapped
 end
+
+function Oken:ColorGradient(perc,...)
+	if perc >= 1 then
+		local r, g, b = select(select('#', ...) - 2, ...)
+		return r, g, b
+	elseif perc <= 0 then
+		local r, g, b = ...
+		return r, g, b
+	end
+
+	local num = select('#', ...) / 3
+
+	local segment, relperc = math.modf(perc * (num - 1))
+	local r1, g1, b1, r2, g2, b2 = select((segment * 3) + 1, ...)
+
+	return r1 + (r2 - r1) * relperc, g1 + (g2 - g1) * relperc, b1 + (b2 - b1) * relperc
+end
